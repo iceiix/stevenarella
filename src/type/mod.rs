72e27968eb1fb5a30dad3091cce6e42be75494f1
Thinkofdeath <thinkofdeath@spigotmc.org>
2015-09-17 16:21:56 +0100

    Add license

--- src/type/mod.rs
+++
@@ -1,6 +0,0 @@
-
-mod blockpos;
-pub use self::blockpos::*;
-
-mod metadata;
-pub use self::metadata::*;
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -11,13 +11,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-use render::glsl;
-
-pub fn add_shaders(reg: &mut glsl::Registry) {
-	reg.register("lookup_texture", include_str!("shaders/lookup_texture.glsl"));
-	reg.register("get_light", include_str!("shaders/get_light.glsl"));
-
-	reg.register("ui_vertex", include_str!("shaders/ui_vertex.glsl"));
-	reg.register("ui_frag", include_str!("shaders/ui_frag.glsl"));
-}
