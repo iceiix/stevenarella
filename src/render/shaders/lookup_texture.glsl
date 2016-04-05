c21a74007740e21826f3d8cfebcbebf646005683
Thinkofname <thinkofdeath@spigotmc.org>
2016-04-05 22:40:35 +0100

    Fix texture seams (Fixes #23)

--- src/render/shaders/lookup_texture.glsl
+++ src/render/shaders/lookup_texture.glsl
@@ -1,7 +1,7 @@
 const float invAtlasSize = 1.0 / 1024;
 vec4 atlasTexture() {
     vec2 tPos = vTextureOffset;
-    tPos = mod(tPos, vTextureInfo.zw);
+    tPos = clamp(tPos, vec2(0.1), vTextureInfo.zw - 0.1);
     tPos += vTextureInfo.xy;
     tPos *= invAtlasSize;
     return texture(textures, vec3(tPos, vAtlas));
