8ca41dfcdab066f5d64913b621ef4e43957dc77f
ice_iix <ice_ix@protonmail.ch>
2018-09-30 00:16:16 -0700

    Update gl-generator, fixes hundreds of non_upper_case_globals warnings
    Resolves most of https://github.com/iceiix/steven/issues/1

--- gl/build.rs
+++ gl/build.rs
@@ -5,15 +5,18 @@ use std::env;
 use std::fs::File;
 use std::io::BufWriter;
 use std::path::Path;
+use gl_generator::{Registry, Api, Profile, Fallbacks, GlobalGenerator};
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest = Path::new(&out_dir);
 
     let mut file = BufWriter::new(File::create(&dest.join("bindings.rs")).unwrap());
-    gl_generator::generate_bindings(gl_generator::GlobalGenerator,
-                                    gl_generator::registry::Ns::Gl,
-                                    gl_generator::Fallbacks::All,
-                                    khronos_api::GL_XML, vec![], "3.2", "core",
-                                    &mut file).unwrap();
+    Registry::new(Api::Gl,
+                  (3, 2),
+                  Profile::Core,
+                  Fallbacks::All,
+                  [])
+        .write_bindings(GlobalGenerator, &mut file)
+        .unwrap();
 }
