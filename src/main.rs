c36d0706ace9a620365c983f47617e905adc6e70
ice_iix <ice_ix@protonmail.ch>
2018-10-29 17:10:08 -0700

    Remove obsolete comment

--- src/main.rs
+++ src/main.rs
@@ -179,7 +179,6 @@ fn main() {
 
     let proxy = console::ConsoleProxy::new(con.clone());
 
-    // TODO: fix error[E0423]: expected function, found macro `log::set_boxed_logger`
     log::set_boxed_logger(Box::new(proxy)).unwrap();
     log::set_max_level(log::LevelFilter::Trace);
 
