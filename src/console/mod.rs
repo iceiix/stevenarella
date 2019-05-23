0c6c8675a94ad0a798dc8b8d7d1167ab2c5c4101
ice_iix <ice_ix@protonmail.ch>
2019-05-23 14:49:06 -0700

    Add a std::fs replacement minimal no-op for wasm

--- src/console/mod.rs
+++ src/console/mod.rs
@@ -17,7 +17,40 @@ use std::collections::HashMap;
 use std::any::Any;
 use std::cell::{RefCell, Ref};
 use std::sync::{Arc, Mutex};
-use std::fs;
+use cfg_if::cfg_if;
+cfg_if! {
+    if #[cfg(target_arch = "wasm32")] {
+        mod fs {
+            use log::info;
+            pub struct File {}
+            impl File {
+                pub fn open(path: &str) -> std::io::Result<File> {
+                    info!("fs open {}", path);
+                    Ok(File{})
+                }
+                pub fn create(path: &str) -> std::io::Result<File> {
+                    info!("fs create {}", path);
+                    Ok(File{})
+                }
+            }
+            impl std::io::Read for File {
+                fn read(&mut self, _buf: &mut [u8]) -> std::io::Result<usize> {
+                    Ok(0)
+                }
+            }
+            impl std::io::Write for File {
+                fn write(&mut self, _buf: &[u8]) -> std::io::Result<usize> {
+                    Ok(0)
+                }
+                fn flush(&mut self) -> std::io::Result<()> {
+                    Ok(())
+                }
+            }
+        }
+    } else {
+        use std::fs;
+    }
+}
 use std::io::{BufWriter, Write, BufRead, BufReader};
 use log;
 
