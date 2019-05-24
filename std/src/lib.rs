6f7f5a919464ee6c212846bfa1bbf552655b5537
ice_iix <ice_ix@protonmail.ch>
2019-05-23 19:49:53 -0700

    Import std::io Result, Read, Write

--- std/src/lib.rs
+++ std/src/lib.rs
@@ -1,26 +1,28 @@
+use log::info;
+use std::io::{Result, Read, Write};
+
 pub mod fs {
-    use log::info;
     pub struct File {}
     impl File {
-        pub fn open(path: &str) -> std::io::Result<File> {
+        pub fn open(path: &str) -> Result<File> {
             info!("fs open {}", path);
             Ok(File{})
         }
-        pub fn create(path: &str) -> std::io::Result<File> {
+        pub fn create(path: &str) -> Result<File> {
             info!("fs create {}", path);
             Ok(File{})
         }
     }
-    impl std::io::Read for File {
-        fn read(&mut self, _buf: &mut [u8]) -> std::io::Result<usize> {
+    impl Read for File {
+        fn read(&mut self, _buf: &mut [u8]) -> Result<usize> {
             Ok(0)
         }
     }
-    impl std::io::Write for File {
-        fn write(&mut self, _buf: &[u8]) -> std::io::Result<usize> {
+    impl Write for File {
+        fn write(&mut self, _buf: &[u8]) -> Result<usize> {
             Ok(0)
         }
-        fn flush(&mut self) -> std::io::Result<()> {
+        fn flush(&mut self) -> Result<()> {
             Ok(())
         }
     }
