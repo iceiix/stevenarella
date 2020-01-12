2d91fb1c7e2471d1e4b34e120ecb944e927c64f8
ice_iix <ice_ix@protonmail.ch>
2020-01-12 14:04:59 -0800

    Call in a loop, no difference

--- src/main.rs
+++ src/main.rs
@@ -252,9 +252,11 @@ fn main2() {
 
     gl::init(&window);
 
-    println!("main call test_image_3d");
-    gl::Texture::new().test_image_3d();
-    println!("return4");
+    for i in 0..10 {
+        println!("main call test_image_3d {}", i);
+        gl::Texture::new().test_image_3d();
+        println!("return {}", i);
+    }
 
     let renderer = render::Renderer::new(resource_manager.clone());
     let mut ui_container = ui::Container::new();
