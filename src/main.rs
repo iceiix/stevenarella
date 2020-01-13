aece114d274b678bb36c608b7ec2cbff3fd514eb
ice_iix <ice_ix@protonmail.ch>
2020-01-12 17:53:02 -0800

    Remove loop from main, crashes in main 4 creating texture

--- src/main.rs
+++ src/main.rs
@@ -252,12 +252,6 @@ fn main2() {
 
     gl::init(&window);
 
-    for i in 0..10 {
-        println!("\nmain {}", i);
-        gl::test_image_3d();
-        println!("ok {}", i);
-    }
-
     let renderer = render::Renderer::new(resource_manager.clone());
     println!("main 4"); gl::test_image_3d(); println!("ok");
     let mut ui_container = ui::Container::new();
