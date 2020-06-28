f857f89d976b2488e93f2be92fc41064f54d5037
ice_iix <ice_ix@protonmail.ch>
2020-06-28 11:08:52 -0700

    Revert "Attempt to disable clippy warnings in generated code, but cannot use top-level annotations here"
    This reverts commit 389df39b0c9cccd66b28b4b666b0133e44243a0c.

--- gl/build.rs
+++ gl/build.rs
@@ -2,7 +2,6 @@ use gl_generator::{Api, Fallbacks, GlobalGenerator, Profile, Registry};
 use std::env;
 use std::fs::File;
 use std::io::BufWriter;
-use std::io::Write;
 use std::path::Path;
 
 fn main() {
@@ -10,8 +9,6 @@ fn main() {
     let dest = Path::new(&out_dir);
 
     let mut file = BufWriter::new(File::create(&dest.join("bindings.rs")).unwrap());
-    file.write_all(b"#![allow(clippy::all)]\n").unwrap();
-
     Registry::new(Api::Gl, (3, 2), Profile::Core, Fallbacks::All, [])
         .write_bindings(GlobalGenerator, &mut file)
         .unwrap();
