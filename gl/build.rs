389df39b0c9cccd66b28b4b666b0133e44243a0c
ice_iix <ice_ix@protonmail.ch>
2020-06-28 11:08:46 -0700

    Attempt to disable clippy warnings in generated code, but cannot use top-level annotations here

--- gl/build.rs
+++ gl/build.rs
@@ -2,6 +2,7 @@ use gl_generator::{Api, Fallbacks, GlobalGenerator, Profile, Registry};
 use std::env;
 use std::fs::File;
 use std::io::BufWriter;
+use std::io::Write;
 use std::path::Path;
 
 fn main() {
@@ -9,6 +10,8 @@ fn main() {
     let dest = Path::new(&out_dir);
 
     let mut file = BufWriter::new(File::create(&dest.join("bindings.rs")).unwrap());
+    file.write_all(b"#![allow(clippy::all)]\n").unwrap();
+
     Registry::new(Api::Gl, (3, 2), Profile::Core, Fallbacks::All, [])
         .write_bindings(GlobalGenerator, &mut file)
         .unwrap();
