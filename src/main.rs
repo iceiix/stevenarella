adaa63288d727b93263e09a1cdbddc4f956641ce
ice_iix <ice_ix@protonmail.ch>
2020-12-27 16:16:49 -0800

    Enable render loop on wasm, disable events_loop on wasm for now

--- src/main.rs
+++ src/main.rs
@@ -399,7 +399,6 @@ fn main2() {
 
     let mut last_resource_version = 0;
 
-    /* TODO: fix move of winit_window
     #[cfg(target_arch = "wasm32")]
     render_loop.run(move |running: &mut bool| {
         tick_all(
@@ -413,8 +412,9 @@ fn main2() {
         );
         println!("render_loop");
     });
-    */
 
+    // TODO: enable events_loop for wasm, too, fix borrow with render_loop
+    #[cfg(not(target_arch = "wasm32"))]
     events_loop.run(move |event, _event_loop, control_flow| {
         #[cfg(target_arch = "wasm32")]
         {
