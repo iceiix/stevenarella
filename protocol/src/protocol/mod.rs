e5c036181251f2cadb0194a9e491c21e8f6a9c0a
ice_iix <ice_ix@protonmail.ch>
2022-08-06 17:02:46 -0700

    cargo fmt

--- protocol/src/protocol/mod.rs
+++ protocol/src/protocol/mod.rs
@@ -40,8 +40,8 @@ use std::net::TcpStream;
 use std::sync::atomic::{AtomicBool, AtomicI32, Ordering};
 
 pub const SUPPORTED_PROTOCOLS: [i32; 27] = [
-    758, 757, 756, 754, 753, 751, 736, 735, 578, 575, 498, 490, 485, 480, 477, 452, 451, 404, 340, 316,
-    315, 210, 109, 107, 74, 47, 5,
+    758, 757, 756, 754, 753, 751, 736, 735, 578, 575, 498, 490, 485, 480, 477, 452, 451, 404, 340,
+    316, 315, 210, 109, 107, 74, 47, 5,
 ];
 
 static CURRENT_PROTOCOL_VERSION: AtomicI32 = AtomicI32::new(SUPPORTED_PROTOCOLS[0]);
