4701f639c8b5742699baaceeeb94e6781cbd71bd
ice_iix <ice_ix@protonmail.ch>
2022-08-28 07:25:54 -0700

    Fix clippy::needless_late_init in load_block_entities

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1947,13 +1947,12 @@ impl Server {
             let z = block_entity.1.get("z").unwrap().as_int().unwrap();
             if let Some(tile_id) = block_entity.1.get("id") {
                 let tile_id = tile_id.as_str().unwrap();
-                let action;
-                match tile_id {
+                let action = match tile_id {
                     // Fake a sign update
-                    "Sign" => action = 9,
+                    "Sign" => 9,
                     // Not something we care about, so break the loop
                     _ => continue,
-                }
+                };
                 self.on_block_entity_update_u8(packet::play::clientbound::UpdateBlockEntity_u8 {
                     location: Position::new(x, y, z),
                     action,
